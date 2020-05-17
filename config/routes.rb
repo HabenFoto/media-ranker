37cc52f977a5487ece351ba82f6e041f58456844
Haben <haben7@gmail.com>
2020-05-17 15:51:35 -0700

    add route for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :works
   root "pages#index"
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
