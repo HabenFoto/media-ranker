07db928367dd0671caa679870e5ef70d488e76d9
Haben <haben7@gmail.com>
2020-05-19 20:05:17 -0700

    added validation test for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :works
   root "pages#index"
-  resources :users
+  resources :users, only: %i[index show new create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 describe User do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+  before do
+    # arrange
+    @user = User.new(username: 'Haben')
+  end
+
+  it 'is valid when there is a username' do
+
+    # act & assert
+    expect(@user.valid?).must_equal true
+  end
+
+  it 'is invalid when there is no username' do
+
+    # act & assert
+    expect(@user.valid?).must_equal true
+   
+    # act
+    @user.username = nil
+
+    # assert
+    expect(@user.valid?).must_equal false
+    expect(@user.errors.messages).must_include :username
+  end
+  
+
 end
