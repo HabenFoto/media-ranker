7e2b6cbab929a8c3ebc0733fe435a2580ec8c7f5
Haben <haben7@gmail.com>
2020-05-19 18:45:54 -0700

    add validation for work model

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,20 @@
 class Work < ApplicationRecord
   has_many :votes
   has_many :users, through: :votes
+  validates :title, presence: true, uniqueness: {scope: :category}
+  
+
+
+  def self.books 
+    Work.where(category: :book)
+  end
+
+  def self.albums
+    Work.where(category: :album)
+  end
+
+  def self.movies
+    Work.where(category: :movie)
+  end
 
 end
