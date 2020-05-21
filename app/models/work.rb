be51d135019fe4b08a1af3d69074d900a34929f3
Haben <haben7@gmail.com>
2020-05-21 16:42:09 -0700

    added .order method to put the votes in order & for spotlight to show the top voted

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,19 +2,22 @@ class Work < ApplicationRecord
   has_many :votes
   has_many :users, through: :votes
   validates :title, presence: true, uniqueness: {scope: :category}
-  
- # need to get the top most voted to spotlight
+
 
   def self.books
-    Work.where(category: :book).left_joins(:users).group(:id).order('COUNT(users) DESC')
+    Work.where(category: :book).left_joins(:votes).group(:id).order('COUNT(votes) DESC, TITLE ASC')
   end
 
   def self.albums
-    Work.where(category: :album).left_joins(:users).group(:id).order('COUNT(users) DESC')
+    Work.where(category: :album).left_joins(:votes).group(:id).order('COUNT(votes) DESC, TITLE ASC')
   end
 
   def self.movies
-    Work.where(category: :movie).left_joins(:users).group(:id).order('COUNT(users) DESC')
+    Work.where(category: :movie).left_joins(:votes).group(:id).order('COUNT(votes) DESC, TITLE ASC')
+  end
+
+  def self.spotlight
+    Work.left_joins(:votes).group(:id).order('COUNT(votes) DESC, TITLE ASC').limit(1).first
   end
  
 end
