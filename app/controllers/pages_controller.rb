499d99b5c7418750b3f365a50a2d1363440ac303
Haben <haben7@gmail.com>
2020-05-21 16:43:13 -0700

    added .limit to the categories inorder to get the top 10 voted work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,9 @@
 class PagesController < ApplicationController
 
   def index
-    @books = Work.books
-    @albums= Work.albums
-    @movies = Work.movies
+    @books = Work.books.limit(10)
+    @albums= Work.albums.limit(10)
+    @movies = Work.movies.limit(10)
+    @spotlight = Work.spotlight 
   end
 end
