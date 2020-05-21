0446779e6a57a00d0b972d6d08fdc624349db7a2
Haben <haben7@gmail.com>
2020-05-20 21:15:54 -0700

    added vote order for each category

--- app/models/work.rb
+++ app/models/work.rb
@@ -3,18 +3,18 @@ class Work < ApplicationRecord
   has_many :users, through: :votes
   validates :title, presence: true, uniqueness: {scope: :category}
   
+ # need to get the top most voted to spotlight
 
-
-  def self.books 
-    Work.where(category: :book)
+  def self.books
+    Work.where(category: :book).left_joins(:users).group(:id).order('COUNT(users) DESC')
   end
 
   def self.albums
-    Work.where(category: :album)
+    Work.where(category: :album).left_joins(:users).group(:id).order('COUNT(users) DESC')
   end
 
   def self.movies
-    Work.where(category: :movie)
+    Work.where(category: :movie).left_joins(:users).group(:id).order('COUNT(users) DESC')
   end
-
+ 
 end
