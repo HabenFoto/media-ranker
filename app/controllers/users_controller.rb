d56001876a6b4520eaab21d6e10c27c1c3d92526
Haben <haben7@gmail.com>
2020-05-20 21:19:54 -0700

    added user index, show, login_form, login, logout & current actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,53 @@
 class UsersController < ApplicationController
+  # before_action :set_work, only: [:show, :edit, :update, :destroy, :upvote]
 
   def index
-   @users = User.all
+    @user = User.all
+  end
+
+  def show
+    @user= current_user
+    if @user.nil?
+      redirect_to users_path
+      return
+    end
+
   end
 
 
+  def login_form
+    @user = User.new
+  end
 
-  def show
+  def login
+    username = params[:user][:username]
+    user = User.find_by(username: username)
+    if user
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as returning user #{username}"
+    else
+      user = User.create(username: username)
+      session[:user_id] = user.id
+      flash[:success] = "Successfully logged in as new user #{username}"
+    end
+
+    redirect_to root_path
+    return
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out"
+    redirect_to root_path
+    return
+  end
+
+  def current
+    @current_user = User.find_by(id: session[:user_id])
+    unless @current_user
+      flash[:error] = "You must be logged in to see this page"
+      redirect_to root_path
+      return
+    end
   end
 end
