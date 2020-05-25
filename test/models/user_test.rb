dc0cce1bbcde8db1f892386643a945d9e53c757d
Haben <haben7@gmail.com>
2020-05-24 23:25:34 -0700

    added relationship tests for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 describe User do
-
+   
+  describe 'validations' do
   before do
     # arrange
     @user = User.new(username: 'Haben')
@@ -15,7 +16,7 @@ describe User do
 
   it 'is invalid when there is no username' do
 
-    # act & assert
+    # arrange
     expect(@user.valid?).must_equal true
    
     # act
@@ -25,6 +26,34 @@ describe User do
     expect(@user.valid?).must_equal false
     expect(@user.errors.messages).must_include :username
   end
+  end
+
+
+  describe "relationships" do
+  let (:new_work) {
+    Work.create(category: "movie", title: "Get Out", creator: "Jordan Peele", year: "2017", description: "A Scarey and Funny movie")
+  }
+  let (:work_two) {
+    Work.create(category: "book", title: "Becoming", creator: "Michelle Obama", year: "2018", description: "Life")
+  }
+
+  let (:user_one) {
+    User.create(username: "hello")
+  }
+
+ 
+    it "can have many votes" do
+      vote_1 = Vote.create(user_id: user_one.id, work_id: work_two.id)
+      vote_2 = Vote.create(user_id: user_one.id, work_id: new_work.id)
+      
+      expect(user_one.votes.count).must_equal 2
+      expect(user_one.votes.first).must_be_instance_of Vote
+    end
+  end
+end
+
   
 
-end
+
+
+
